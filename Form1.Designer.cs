031d71da8d6c8fe1ec83adf8c081a3b20b04b64c
huhu9001 <huhu9001@163.com>
2022-05-21 18:54:33 +0800

    000

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace LoveStringH
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_main = new System.Windows.Forms.TextBox();
+            this.tb_byte = new System.Windows.Forms.TextBox();
+            this.cb_escapeStyle = new System.Windows.Forms.ComboBox();
+            this.cb_encoding = new System.Windows.Forms.ComboBox();
+            this.ch_keepText = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // tb_main
+            // 
+            this.tb_main.Location = new System.Drawing.Point(15, 12);
+            this.tb_main.Multiline = true;
+            this.tb_main.Name = "tb_main";
+            this.tb_main.Size = new System.Drawing.Size(773, 128);
+            this.tb_main.TabIndex = 0;
+            this.tb_main.TextChanged += new System.EventHandler(this.tb_main_TextChanged);
+            // 
+            // tb_byte
+            // 
+            this.tb_byte.Location = new System.Drawing.Point(15, 178);
+            this.tb_byte.Multiline = true;
+            this.tb_byte.Name = "tb_byte";
+            this.tb_byte.Size = new System.Drawing.Size(773, 172);
+            this.tb_byte.TabIndex = 9;
+            this.tb_byte.TextChanged += new System.EventHandler(this.tb_byte_TextChanged);
+            // 
+            // cb_escapeStyle
+            // 
+            this.cb_escapeStyle.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb_escapeStyle.FormattingEnabled = true;
+            this.cb_escapeStyle.Location = new System.Drawing.Point(150, 146);
+            this.cb_escapeStyle.Name = "cb_escapeStyle";
+            this.cb_escapeStyle.Size = new System.Drawing.Size(125, 26);
+            this.cb_escapeStyle.TabIndex = 16;
+            this.cb_escapeStyle.SelectedIndexChanged += new System.EventHandler(this.cb_escapeStyle_SelectedIndexChanged);
+            // 
+            // cb_encoding
+            // 
+            this.cb_encoding.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cb_encoding.FormattingEnabled = true;
+            this.cb_encoding.Location = new System.Drawing.Point(15, 146);
+            this.cb_encoding.Name = "cb_encoding";
+            this.cb_encoding.Size = new System.Drawing.Size(129, 26);
+            this.cb_encoding.TabIndex = 17;
+            this.cb_encoding.SelectedIndexChanged += new System.EventHandler(this.cb_encoding_SelectedIndexChanged);
+            // 
+            // ch_keepText
+            // 
+            this.ch_keepText.AutoSize = true;
+            this.ch_keepText.Checked = true;
+            this.ch_keepText.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ch_keepText.Location = new System.Drawing.Point(299, 148);
+            this.ch_keepText.Name = "ch_keepText";
+            this.ch_keepText.Size = new System.Drawing.Size(205, 22);
+            this.ch_keepText.TabIndex = 18;
+            this.ch_keepText.Text = "Keep Text Unchanged";
+            this.ch_keepText.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 362);
+            this.Controls.Add(this.ch_keepText);
+            this.Controls.Add(this.cb_encoding);
+            this.Controls.Add(this.cb_escapeStyle);
+            this.Controls.Add(this.tb_byte);
+            this.Controls.Add(this.tb_main);
+            this.Name = "Form1";
+            this.Text = "LoveStringH";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_main;
+        private System.Windows.Forms.TextBox tb_byte;
+        private System.Windows.Forms.ComboBox cb_escapeStyle;
+        private System.Windows.Forms.ComboBox cb_encoding;
+        private System.Windows.Forms.CheckBox ch_keepText;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Text.RegularExpressions;
+
+namespace LoveStringH
+{
+    public partial class Form1 : Form {
+        struct EscapeStyle {
+            public delegate string f(uint u);
+
+            public string name { get; set; }
+            public f f_this;
+        }
+        EscapeStyle[] EscapeStyles_default = new EscapeStyle[] {
+            new EscapeStyle{ name = "\\x", f_this = (uint u) => { return String.Format("\\x{0:X}", u); } },
+            new EscapeStyle{ name = "%", f_this = (uint u) => { return String.Format("%{0:X2}", u); } },
+        };
+
+        struct EncoderItem {
+            public string name { get; set; }
+            public Encoding e;
+            public EscapeStyle[] styles;
+        };
+        EncoderItem[] EncoderItems = new EncoderItem[] {
+            new EncoderItem{ name = "Unicode", e = null, styles = new EscapeStyle[] {
+                new EscapeStyle{ name = "\\u", f_this = (uint u) => {
+                    if (u < 0x80) return String.Format("\\x{0:X}", u);
+                    if (u < 0x10000) return String.Format("\\u{0:X4}", u);
+                    return String.Format("\\U{0:X8}", u);
+                } },
+                new EscapeStyle{ name = "&#x", f_this = (uint u) => {
+                    return String.Format("&#x{0:X};", u);
+                } },
+            } },
+            new EncoderItem{ name = "UTF-8", e = Encoding.UTF8 },
+            new EncoderItem{ name = "UTF-16", e = null, styles = new EscapeStyle[] {
+                new EscapeStyle{ name = "\\x", f_this = (uint u) => { return String.Format("\\x{0:X}", u); } },
+            } },
+            new EncoderItem{ name = "GB 18030", e = Encoding.GetEncoding(54936) },
+            new EncoderItem{ name = "Shift-JIS", e = Encoding.GetEncoding(932) },
+            new EncoderItem{ name = "Big5", e = Encoding.GetEncoding(950) },
+            new EncoderItem{ name = "EUC-JP", e = Encoding.GetEncoding(51932) },
+        };
+
+        public Form1() {
+            InitializeComponent();
+
+            cb_encoding.DataSource = EncoderItems;
+            cb_encoding.DisplayMember = "name";
+            cb_encoding.SelectedIndex = 0;
+
+            cb_escapeStyle.DisplayMember = "name";
+            cb_encoding_SelectedIndexChanged(null, null);
+        }
+
+        private void tb_main_TextChanged(object sender, EventArgs e) {
+            Encoding ec = ((EncoderItem)cb_encoding.SelectedItem).e;
+            StringBuilder s_result = new StringBuilder();
+            if (ec == null) {
+                byte[] bs = Encoding.Unicode.GetBytes(tb_main.Text);
+                for (uint u0 = 0; u0 < bs.Length; u0 += 2) {
+                    uint u_char = bs[u0] + ((uint)bs[u0 + 1] << 8); 
+                    if (cb_encoding.SelectedIndex == 0 && u_char >= 0xD800 && u_char < 0xDC00) {
+                        u0 += 2;
+                        uint u_char_2 = bs[u0] + ((uint)bs[u0 + 1] << 8);
+                        u_char = (u_char - 0xD7C0 << 10) + (u_char_2 - 0xDC00);
+                    }
+                    s_result.Append(((EscapeStyle)cb_escapeStyle.SelectedItem).f_this(u_char));
+                }
+            }
+            else {
+                byte[] bs = ec.GetBytes(tb_main.Text);
+                foreach(byte b in bs) {
+                    s_result.Append(((EscapeStyle)cb_escapeStyle.SelectedItem).f_this(b));
+                }
+            }
+
+            tb_byte.TextChanged -= tb_byte_TextChanged;
+            tb_byte.Text = s_result.ToString();
+            tb_byte.TextChanged += tb_byte_TextChanged;
+            ch_keepText.Checked = true;
+        }
+
+        private void tb_byte_TextChanged(object sender, EventArgs e) {
+            Encoding ec = ((EncoderItem)cb_encoding.SelectedItem).e;
+            StringBuilder s_result = new StringBuilder();
+            if (ec == null) ec = Encoding.Unicode;
+
+            int i0 = 0;
+            List<byte> bs = new List<byte>();
+            foreach (Match m in Regex.Matches(tb_byte.Text, "\\\\([0-7]{1,3})|\\\\x([A-Fa-f0-9]+)|%([A-Fa-f0-9]{2})|\\\\u([A-Fa-f0-9]{4})|\\\\U([A-Fa-f0-9]{8})|&#(\\d+);|&#x([A-Fa-f0-9]+);")) {
+                if (i0 < m.Index) {
+                    try { s_result.Append(ec.GetChars(bs.ToArray())); }
+                    catch (DecoderFallbackException) { s_result.Append('?'); }
+                    bs.Clear();
+                    s_result.Append(tb_byte.Text.Substring(i0, m.Index - i0));
+                }
+
+                uint u_char;
+                if (m.Groups[1].Success) u_char = Convert.ToUInt32(m.Groups[1].Value, 8);
+                else if (m.Groups[2].Success) u_char = Convert.ToUInt32(m.Groups[2].Value, 16);
+                else if (m.Groups[3].Success) u_char = Convert.ToUInt32(m.Groups[3].Value, 16);
+                else {
+                    try { s_result.Append(ec.GetChars(bs.ToArray())); }
+                    catch (DecoderFallbackException) { s_result.Append('?'); }
+                    bs.Clear();
+
+                    if (m.Groups[4].Success) u_char = Convert.ToUInt32(m.Groups[4].Value, 16);
+                    else if (m.Groups[5].Success) u_char = Convert.ToUInt32(m.Groups[5].Value, 16);
+                    else if (m.Groups[6].Success) u_char = Convert.ToUInt32(m.Groups[6].Value, 10);
+                    else u_char = Convert.ToUInt32(m.Groups[7].Value, 16);
+
+                    if (u_char < 0x10000) s_result.Append((char)u_char);
+                    else {
+                        s_result.Append((char)((u_char >> 10) + 0xD7C0));
+                        s_result.Append((char)((u_char & 0x3FF) + 0xDC00));
+                    }
+                    i0 = m.Index + m.Length;
+                    continue;
+                }
+
+                if (ec == Encoding.Unicode) {
+                    while (u_char > 0xFFFF) {
+                        bs.Add((byte)(u_char & 0xFF));
+                        bs.Add((byte)(u_char >> 8 & 0xFF));
+                        u_char >>= 16;
+                    }
+                    bs.Add((byte)(u_char & 0xFF));
+                    bs.Add((byte)(u_char >> 8 & 0xFF));
+                }
+                else {
+                    while (u_char > 0xFF) {
+                        bs.Add((byte)(u_char & 0xFF));
+                        u_char >>= 8;
+                    }
+                    bs.Add((byte)(u_char & 0xFF));
+                }
+                i0 = m.Index + m.Length;
+            }
+            try { s_result.Append(ec.GetChars(bs.ToArray())); }
+            catch (DecoderFallbackException) { s_result.Append('?'); }
+            if (i0 < tb_byte.Text.Length) s_result.Append(tb_byte.Text.Substring(i0));
+
+            tb_main.TextChanged -= tb_main_TextChanged;
+            tb_main.Text = s_result.ToString();
+            tb_main.TextChanged += tb_main_TextChanged;
+            ch_keepText.Checked = false;
+        }
+
+        private void cb_encoding_SelectedIndexChanged(object sender, EventArgs e) {
+            EscapeStyle[] styles = ((EncoderItem)cb_encoding.SelectedItem).styles;
+            cb_escapeStyle.SelectedIndexChanged -= cb_escapeStyle_SelectedIndexChanged;
+            if (styles == null) cb_escapeStyle.DataSource = EscapeStyles_default;
+            else cb_escapeStyle.DataSource = styles;
+            cb_escapeStyle.SelectedIndexChanged += cb_escapeStyle_SelectedIndexChanged;
+
+            if (ch_keepText.Checked) tb_main_TextChanged(sender, e);
+            else tb_byte_TextChanged(sender, e);
+        }
+
+        private void cb_escapeStyle_SelectedIndexChanged(object sender, EventArgs e) {
+            tb_main_TextChanged(sender, e);
+        }
+    }
+}
